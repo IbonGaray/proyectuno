5751cc90734d87d0f680a11bcc71aee85c18c817
Ibon Garay <ibon.bengoa@opendeusto.es>
2016-02-27 13:41:09 +0100

    27022016 - 1341

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.uno, name='uno'),
-    url(r'^dos/$', views.dos, name='dos'),
-    url(r'^gijon/gastos/$', views.gijonGastos, name='gijonGastos'),
-    url(r'^cuatrocuatro/$', views.cuatrocuatro, name='cuatrocuatro'),
+    url(r'^santander/$', views.santander, name='santander'),
+    url(r'^gijon/gastos/tiempo/$', views.gijonGastosTiempo, name='gijonGastosTiempo'),
+    url(r'^gijon/gastos/economico/$', views.gijonGastosEconomico, name='gijonGastosEconomico'),
+    url(r'^gijon/gastos/capitulo/$', views.gijonGastosCapitulo, name='gijonGastosCapitulo'),
+    url(r'^gijon/gastos/funcional/$', views.gijonGastosFuncional, name='gijonGastosFuncional'),
+    url(r'^gijon/gastos/articulo/$', views.gijonGastosArticulo, name='gijonGastosArticulo'),
+    url(r'^gijon/gastos/partida/$', views.gijonGastosPartida, name='gijonGastosPartida'),
+
+
+    url(r'^gijon/ingresos/$', views.gijonIngresos, name='gijonIngresos'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 
 def uno(request):
     return render(request, 'uno.html', {})
-def dos(request):
-    return render(request, 'dos.html', {})
-def gijonGastos(request):
-    return render(request, 'gijonGastos.html', {})
-def cuatrocuatro(request):
-    return render(request, 'cuatrocuatro.html', {})
+def santander(request):
+    return render(request, 'santander.html', {})
+def gijonGastosTiempo(request):
+    return render(request, 'gijonGastosTiempo.html', {})
+def gijonGastosEconomico(request):
+    return render(request, 'gijonGastosEconomico.html', {})
+def gijonGastosCapitulo(request):
+    return render(request, 'gijonGastosCapitulo.html', {})
+def gijonGastosFuncional(request):
+    return render(request, 'gijonGastosFuncional.html', {})
+def gijonGastosArticulo(request):
+    return render(request, 'gijonGastosArticulo.html', {})
+def gijonGastosPartida(request):
+    return render(request, 'gijonGastosPartida.html', {})
+def gijonIngresos(request):
+    return render(request, 'gijonIngresos.html', {})
